1f53f78884df3f159c549942c3460354377596dd
Srizan <srizan.jatri@gmail.com>
2022-05-22 10:29:26 +0600

    "Done" button added in Secret password text input and action added...

--- app/src/main/java/com/jatri/offlinecounterticketing/ui/secretpassword/SecretPassword.kt
+++ app/src/main/java/com/jatri/offlinecounterticketing/ui/secretpassword/SecretPassword.kt
@@ -1,6 +1,8 @@
 package com.jatri.offlinecounterticketing.ui.secretpassword
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -8,6 +10,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jatri.offlinecounterticketing.ui.components.JatriLogo
@@ -16,7 +19,7 @@ import com.jatri.offlinecounterticketing.ui.theme.OfflineCounterTicketingTheme
 
 @Composable
 fun SecretPassword(
-    onContinueClick : (String) -> Unit
+    onContinueClick: (String) -> Unit
 ) {
     Column(
         modifier = Modifier.fillMaxHeight(),
@@ -32,10 +35,18 @@ fun SecretPassword(
             label = { Text("Secret Password") },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp)
+                .padding(start = 16.dp, end = 16.dp),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    onContinueClick.invoke(text)
+                }
+            ),
         )
         Spacer(modifier = Modifier.size(48.dp))
-        RoundJatriButton("Continue"){
+        RoundJatriButton("Continue") {
             onContinueClick.invoke(text)
         }
     }
@@ -46,7 +57,7 @@ fun SecretPassword(
 fun SecretPasswordPrev() {
     OfflineCounterTicketingTheme {
         Surface {
-            SecretPassword{
+            SecretPassword {
 
             }
         }
--- app/src/main/java/com/jatri/offlinecounterticketing/ui/secretpassword/SecretPasswordFragment.kt
+++ app/src/main/java/com/jatri/offlinecounterticketing/ui/secretpassword/SecretPasswordFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -48,6 +49,9 @@ class SecretPasswordFragment : Fragment() {
             if (secretPassword == resources.getString(R.string.secretPassword)) {
                 findNavController().navigate(SecretPasswordFragmentDirections.actionSecretPasswordFragmentToConfigurationFragment(jsonString))
                 sharedPrefHelper.putBool(SpKey.passwordEntered, true)
+            }
+            else {
+                Toast.makeText(requireContext(),"Wrong Password!", Toast.LENGTH_SHORT).show()
             }
         }
         setContent {
