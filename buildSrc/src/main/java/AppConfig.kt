5a542d19d640363b75b6bd8582382edd756622c4
Srizan <srizan.jatri@gmail.com>
2022-06-01 10:38:24 +0600

    Version updated to 1.0.1

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -4,6 +4,6 @@ object AppConfig {
     const val minSdkVersion = 21
     const val compileSdkVersion = 31
     const val targetSdkVersion = 31
-    const val versionCode  = 1
-    const val versionName  = "1.0.0"
+    const val versionCode  = 2
+    const val versionName  = "1.0.1"
 }
