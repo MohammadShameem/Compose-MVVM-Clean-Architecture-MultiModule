c19ae9ce5ee4503bbd0107a8b063e48670158f08
Srizan <srizan.jatri@gmail.com>
2022-06-05 12:08:18 +0600

    optimized imports

--- common/src/main/java/com/jatri/common/base/BaseFragment.kt
+++ common/src/main/java/com/jatri/common/base/BaseFragment.kt
@@ -9,7 +9,6 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.google.android.material.snackbar.Snackbar
-import com.jatri.common.extfun.hideKeyboard
 
 abstract class BaseFragment<V : ViewBinding> : Fragment() {
     private var _binding: V? = null
